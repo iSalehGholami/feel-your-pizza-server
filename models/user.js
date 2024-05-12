c7c7ea8006baf5f6a6179f1c698c1f2b540f4417
Mo. Saleh Gholami <iSalehGholami@gmail.com>
2024-05-13 02:24:41 +0330

    creating usermodel

---
+++ models/user.js
@@ -0,0 +1,42 @@
+// models/user.js
+
+const { DataTypes } = require('sequelize');
+const sequelize = require('./index');
+const Pizza = require('./pizza');
+
+const User = sequelize.define('User', {
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password_hash: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    mood: {
+        type: DataTypes.ENUM('happy', 'hungry', 'bored', 'furious', 'sad'),
+        allowNull: false
+    },
+    favoritePizzas: {
+        type: DataTypes.ARRAY(DataTypes.INTEGER),
+        defaultValue: [] // Default value is an empty array
+    },
+    pizzaHistory: {
+        type: DataTypes.ARRAY(DataTypes.INTEGER),
+        defaultValue: [] // Default value is an empty array
+    }
+});
+
+// Define association with Pizza model for favorite pizzas
+User.belongsToMany(Pizza, { through: 'UserFavoritePizzas', as: 'FavoritePizzas' });
+
+// Define association with Pizza model for pizza history
+User.belongsToMany(Pizza, { through: 'UserPizzaHistory', as: 'PizzaHistory' });
+
+module.exports = User;
